b4771427d8fae3bbe1d04d959deb3a74eaf9fbf9
Jacques Carette <carette@mcmaster.ca>
2016-05-28 09:47:57 -0400

    replace Unit by a payload from some arbitrary type.  Need an Eq constraint for now, but will make that disappear next.

--- AM.hs
+++ AM.hs
@@ -5,46 +5,54 @@ import Debug.Trace
 
 -- Abstract Machine for PI/
 
-data Combinator = Zeroe | Zeroi
+data Combinator a = 
+                  Zeroe | Zeroi
                 | SwapPlus 
                 | AssoclPlus | AssocrPlus
-                | Unite | Uniti
+                | Unite a | Uniti a
                 | SwapTimes 
                 | AssoclTimes | AssocrTimes
                 | DistribZero | FactorZero
                 | Distrib | Factor
-                | Id | Sym Combinator | Seq Combinator Combinator
-                | Plus Combinator Combinator | Times Combinator Combinator
-                | Eta Combinator | Epsilon Combinator
+                | Id | Sym (Combinator a) 
+                | Seq (Combinator a) (Combinator a)
+                | Plus (Combinator a) (Combinator a) 
+                | Times (Combinator a) (Combinator a)
+                | Eta (Combinator a) | Epsilon (Combinator a) a
                 | Test
   deriving (Eq,Show)
 
-ceq :: Combinator -> Combinator -> Bool
+ceq :: Eq a => Combinator a -> Combinator a -> Bool
 ceq (Seq Test Test) Id = True
 ceq Id (Seq Test Test) = True
 ceq c1 c2 = c1 == c2
 
-data Value = Unit | Inj1 Value | Inj2 Value | Pair Value Value
-           | Comb Combinator | Recip Combinator
+data Value a = Unit a | Inj1 (Value a) | Inj2 (Value a) 
+             | Pair (Value a) (Value a)
+             | Comb (Combinator a) | Recip (Combinator a)
   deriving (Eq,Show)
 
-data Context = Empty
-             | Fst Context Combinator | Snd Combinator Context
-             | LeftTimes Context Combinator Value | RightTimes Combinator Value Context
-             | LeftPlus Context Combinator | RightPlus Combinator Context
+data Context a = 
+    Empty
+  | Fst (Context a) (Combinator a) | Snd (Combinator a) (Context a)
+  | LeftTimes (Context a) (Combinator a) (Value a) 
+  | RightTimes (Combinator a) (Value a) (Context a)
+  | LeftPlus (Context a) (Combinator a) | RightPlus (Combinator a) (Context a)
   deriving (Eq,Show)
 
-data State = Enter Combinator Value Context | Exit Combinator Value Context
+data State a = 
+    Enter (Combinator a) (Value a) (Context a) 
+  | Exit (Combinator a) (Value a) (Context a)
   deriving (Eq,Show)
 
 data Dir = Forward | Back
   deriving (Eq,Show)
 
-traceForward :: State -> a -> a
+traceForward :: Show a => State a -> b -> b
 traceForward s@(Enter _ _ _) = trace (">>> " ++ show s ++ "\n\n")
 traceForward _ = id
 
-traceBack :: State -> a -> a
+traceBack :: Show a => State a -> b -> b
 traceBack s@(Exit _ _ _) = trace ("<<< " ++ show s ++ "\n\n")
 traceBack _ = id
 
@@ -52,7 +60,7 @@ fwdS, backS :: String
 fwdS =  "******************* FORWARD *******************\n\n"
 backS = "****************** BACK *******************\n\n"
 
-eval :: State -> Value
+eval :: (Show a, Eq a) => State a -> Value a
 eval = loopForward
   where 
     loopForward s = traceForward s $
@@ -68,7 +76,7 @@ eval = loopForward
          Back    -> loopBack s'
          Forward -> trace fwdS $ loopForward s'
 
-stepForward :: State -> (Dir,State)
+stepForward :: Eq a => State a -> (Dir,State a)
 stepForward (Exit c1 v (Fst k c2)) = (Forward, Enter c2 v (Snd c1 k))
 stepForward (Exit c2 v (Snd c1 k)) = (Forward, Exit (Seq c1 c2) v k)
 stepForward (Exit c1 v (LeftPlus k c2)) = (Forward, Exit (Plus c1 c2) (Inj1 v) k)
@@ -85,8 +93,8 @@ stepForward (Enter AssoclPlus (Inj2 (Inj2 v3)) k) = (Forward, Exit AssoclPlus (I
 stepForward (Enter AssocrPlus (Inj1 (Inj1 v1)) k) = (Forward, Exit AssocrPlus (Inj1 v1) k)
 stepForward (Enter AssocrPlus (Inj1 (Inj2 v2)) k) = (Forward, Exit AssocrPlus (Inj2 (Inj1 v2)) k)
 stepForward (Enter AssocrPlus (Inj2 v3) k) = (Forward, Exit AssocrPlus (Inj2 (Inj2 v3)) k)
-stepForward (Enter Unite (Pair Unit v) k) = (Forward, Exit Unite v k)
-stepForward (Enter Uniti v k) = (Forward, Exit Uniti (Pair Unit v) k)
+stepForward (Enter (Unite z) (Pair (Unit _) v) k) = (Forward, Exit (Unite z) v k)
+stepForward (Enter (Uniti z) v k) = (Forward, Exit (Uniti z) (Pair (Unit z) v) k)
 stepForward (Enter SwapTimes (Pair v1 v2) k) = (Forward, Exit SwapTimes (Pair v2 v1) k)
 stepForward (Enter AssoclTimes (Pair v1 (Pair v2 v3)) k) = (Forward, Exit AssoclTimes (Pair (Pair v1 v2) v3) k)
 stepForward (Enter AssocrTimes (Pair (Pair v1 v2) v3) k) = (Forward, Exit AssocrTimes (Pair v1 (Pair v2 v3)) k)
@@ -99,10 +107,10 @@ stepForward (Enter (Seq c1 c2) v k) = (Forward, Enter c1 v (Fst k c2))
 stepForward (Enter (Plus c1 c2) (Inj1 v) k) = (Forward, Enter c1 v (LeftPlus k c2))
 stepForward (Enter (Plus c1 c2) (Inj2 v) k) = (Forward, Enter c2 v (RightPlus c1 k))
 stepForward (Enter (Times c1 c2) (Pair v1 v2) k) = (Forward, Enter c1 v1 (LeftTimes k c2 v2))
-stepForward (Enter (Eta p) Unit k) = (Forward, Exit (Eta p) (Pair (Comb p) (Recip p)) k)
-stepForward (Enter (Epsilon r) (Pair (Recip q) (Comb p)) k) | ceq p q = (Forward, Exit (Epsilon r) Unit k)
-                                                            | otherwise =
-  (Back, Enter (Epsilon r) (Pair (Recip q) (Comb p)) k)
+stepForward (Enter (Eta p) (Unit _) k) = (Forward, Exit (Eta p) (Pair (Comb p) (Recip p)) k)
+stepForward (Enter (Epsilon r z) (Pair (Recip q) (Comb p)) k) 
+  | ceq p q   = (Forward, Exit (Epsilon r z) (Unit z) k)
+  | otherwise = (Back, Enter (Epsilon r z) (Pair (Recip q) (Comb p)) k)
 -- these cases were previously missing
 stepForward (Enter DistribZero (Pair _ _) _) = 
   error "impossible case (DistribZero), but Haskell can't see that"
@@ -117,7 +125,7 @@ stepForward (Enter Zeroe _ _) = error "ill-typed"
 stepForward (Enter SwapPlus _ _) = error "ill-typed"
 stepForward (Enter AssoclPlus _ _) = error "ill-typed"
 stepForward (Enter AssocrPlus _ _) = error "ill-typed"
-stepForward (Enter Unite _ _) = error "ill-typed"
+stepForward (Enter (Unite _) _ _) = error "ill-typed"
 stepForward (Enter SwapTimes _ _) = error "ill-typed"
 stepForward (Enter AssoclTimes _ _) = error "ill-typed"
 stepForward (Enter AssocrTimes _ _) = error "ill-typed"
@@ -126,10 +134,10 @@ stepForward (Enter Distrib _ _) = error "ill-typed"
 stepForward (Enter (Plus _ _) _ _) = error "ill-typed"
 stepForward (Enter (Times _ _) _ _) = error "ill-typed"
 stepForward (Enter (Eta _) _ _) = error "ill-typed"
-stepForward (Enter (Epsilon _) _ _) = error "ill-typed"
+stepForward (Enter (Epsilon _ _) _ _) = error "ill-typed"
 stepForward (Enter DistribZero _ _) = error "ill-typed"
 
-stepBack :: State -> (Dir,State)
+stepBack :: Eq a => State a -> (Dir,State a)
 stepBack (Enter c1 v (Fst k c2)) = (Back, Enter (Seq c1 c2) v k)
 stepBack (Enter c2 v (Snd c1 k)) = (Back, Exit c1 v (Fst k c2))
 stepBack (Enter c1 v (LeftPlus k c2)) = (Back, Enter (Plus c1 c2) (Inj1 v) k)
@@ -146,8 +154,8 @@ stepBack (Exit AssoclPlus (Inj2 v3) k) = (Back, Enter AssoclPlus (Inj2 (Inj2 v3)
 stepBack (Exit AssocrPlus (Inj1 v1) k) = (Back, Enter AssocrPlus (Inj1 (Inj1 v1)) k)
 stepBack (Exit AssocrPlus (Inj2 (Inj1 v2)) k) = (Back, Enter AssocrPlus (Inj1 (Inj2 v2)) k)
 stepBack (Exit AssocrPlus (Inj2 (Inj2 v3)) k) = (Back, Enter AssocrPlus (Inj2 v3) k)
-stepBack (Exit Unite v k) = (Back, Enter Unite (Pair Unit v) k)
-stepBack (Exit Uniti (Pair Unit v) k) = (Back, Enter Uniti v k)
+stepBack (Exit (Unite z) v k) = (Back, Enter (Unite z) (Pair (Unit z) v) k)
+stepBack (Exit (Uniti z) (Pair (Unit _) v) k) = (Back, Enter (Uniti z) v k)
 stepBack (Exit SwapTimes (Pair v2 v1) k) = (Back, Enter SwapTimes (Pair v1 v2) k)
 stepBack (Exit AssoclTimes (Pair (Pair v1 v2) v3) k) = (Back, Enter AssoclTimes (Pair v1 (Pair v2 v3)) k)
 stepBack (Exit AssocrTimes (Pair v1 (Pair v2 v3)) k) = (Back, Enter AssocrTimes (Pair (Pair v1 v2) v3) k)
@@ -164,7 +172,7 @@ stepBack (Exit (Eta r) (Pair (Comb p) (Recip q)) k) | ceq p q =
   (Forward, Exit (Eta r) (Pair (Comb (Seq r p)) (Recip (Seq r q))) k)
                                                     | otherwise =
   error "Unexpected eta back if we are starting from left"
-stepBack (Exit (Epsilon _) Unit _) =
+stepBack (Exit (Epsilon _ _) (Unit _) _) =
   error "Unexpected epsilon back if we are starting from left"
 stepBack (Exit DistribZero (Pair _ _) _) = 
   error "impossible case (DistribZero), but Haskell can't see that"
@@ -174,7 +182,7 @@ stepBack (Exit (Sym c) v k) = stepForward (Enter c v k)
 stepBack (Exit Test _ _) = error "should never evaluate Test"
 stepBack (Enter _ _ Empty) = error "should not be trying to step back at start"
 stepBack (Exit Zeroi _ _) = error "ill-typed"
-stepBack (Exit Uniti _ _) = error "ill-typed"
+stepBack (Exit (Uniti _) _ _) = error "ill-typed"
 stepBack (Exit SwapPlus _ _) = error "ill-typed"
 stepBack (Exit AssoclPlus _ _) = error "ill-typed"
 stepBack (Exit AssocrPlus _ _) = error "ill-typed"
@@ -186,18 +194,18 @@ stepBack (Exit Distrib _ _) = error "ill-typed"
 stepBack (Exit (Plus _ _) _ _) = error "ill-typed"
 stepBack (Exit (Times _ _) _ _) = error "ill-typed"
 stepBack (Exit (Eta _) _ _) = error "ill-typed"
-stepBack (Exit (Epsilon _) _ _) = error "ill-typed"
+stepBack (Exit (Epsilon _ _) _ _) = error "ill-typed"
 stepBack (Exit DistribZero _ _) = error "ill-typed"
 
 -- Credit Card Example
 
-ex :: Combinator
-ex = foldr1 Seq [Uniti, Times (Eta Test) Id, AssocrTimes, Times Id (Epsilon Test), SwapTimes, Unite]
+ex :: a -> Combinator a
+ex z = foldr1 Seq [Uniti z, Times (Eta Test) Id, AssocrTimes, Times Id (Epsilon Test z), SwapTimes, Unite z]
 
-load :: Combinator -> Value -> State
+load :: Combinator a -> Value a -> State a
 load c v = Enter c v Empty
 
-test1, test2 :: Value
-test1 = eval $ load ex (Comb Test)
-test2 = eval $ load ex (Comb Id)
+test1, test2 :: Value String
+test1 = eval $ load (ex "test1") (Comb Test)
+test2 = eval $ load (ex "test2") (Comb Id)
 
